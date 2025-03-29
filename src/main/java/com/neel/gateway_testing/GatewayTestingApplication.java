2f88f64b177862bb00f24d1b8c8b2b4dc95b50b4
neeld <neeldc2@gmail.com>
2025-03-29 13:03:10 +0530

    Initial commit

---
+++ src/main/java/com/neel/gateway_testing/GatewayTestingApplication.java
@@ -0,0 +1,13 @@
+package com.neel.gateway_testing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayTestingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neel/gateway_testing/GatewayTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neel.gateway_testing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
